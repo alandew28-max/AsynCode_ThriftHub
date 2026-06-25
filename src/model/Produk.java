ac5e20830a161493dbbe3f239a620e6812abd6e4
irul-kafy <khoirulkhahfi60@gmail.com>
2026-06-25 13:56:32 +0700

    Menambahkan model, interface, dan banner

---
+++ src/model/Produk.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Produk {
+
+    private int idProduk;
+    private String namaProduk;
+    private double harga;
+
+    public Produk(int idProduk, String namaProduk, double harga) {
+        this.idProduk = idProduk;
+        this.namaProduk = namaProduk;
+        this.harga = harga;
+    }
+
+    public int getIdProduk() {
+        return idProduk;
+    }
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+    public String tampilInfo() {
+    return namaProduk + " - Rp " + harga;
+}
+  
+    
+}
---
+++ src/model/ProdukBundle.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author LOQ
+ */
+public class ProdukBundle extends Produk{
+    
+    public ProdukBundle(int id, String nama, double harga) {
+        super(id, nama, harga);
+    }
+
+    @Override
+    public String tampilInfo() {
+        return "Produk Bundle : " + getNamaProduk();
+    }
+}
